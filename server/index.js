69b90fe28ec11aa21cc9525f88db6891b551a840
bbli1012 <ben@onetechnology.io>
2020-06-19 15:40:22 -0700

    server setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
-const express =  require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const Product = require('../database/Product.js');
+const User = require('../databse/User.js');
 
 const app = express();
+const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/explore' ,(req,res) => res.send("Hello World"));
+
+app.list(port, () => console.log(`Server listening at http://localhost:${port}`));
